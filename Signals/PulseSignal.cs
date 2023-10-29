f2c644425663e3d9d61040dd9a244f6e00c0e84c
willysteams <100480712+willysteams@users.noreply.github.com>
2023-10-30 01:00:14 +0300

    Add files via upload
    Updating

---
+++ Signals/PulseSignal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+
+namespace DSP_lab2.Signals
+{
+    class PulseSignal : GeneratedSignal
+    {
+        public override SignalTypes SignalType { get; set; } = SignalTypes.Pulse;
+
+        public PulseSignal(float phi0, float f, float a, float d): base(phi0, f, a)
+        {
+            this.d = d;
+        }
+        //init
+        public override PointF[] Generate(float phi0, float A, float f, int N, float? d = 0.5f)
+        {
+            PointF[] points = new PointF[N];
+            for (int n = 0; n < N; n++)
+            {
+                points[n].X = n / (float)N;
+                points[n].Y = (2 * Math.PI * f * n / N + phi0) % (2 * Math.PI) / (2 * Math.PI) <= d ? A : -A;
+            }
+
+            return points;
+        }
+
+        protected override void OnPropertyChanged(string propertyName)
+        {
+            base.OnPropertyChanged(propertyName);
+        }
+    }
+}
---
+++ Signals/SignalTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace DSP_lab2.Signals
+{
+    public enum SignalTypes
+    {
+        Cosine,
+        Sine,
+        Triangle,
+        Sawtooth,
+        Pulse
+    }
+}
